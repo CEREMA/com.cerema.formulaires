061c7906f83023197f4e5cae3fd1c67182f46dd4
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-11 22:03:47 +0200

    Update# 115

--- src/System/app.js
+++ src/System/app.js
@@ -3,7 +3,29 @@ App = {
 		app.use('/tmp',server.static(__dirname + require('path').sep+'tmp'));	
 		app.post('/',app.UPLOAD.any(),function(req,res,next) {
 			App.upload.up(req,res);
-		});        
+		});
+		app.get('/docs/*',function(req,res) {
+			var ff=req.originalUrl.substr(req.originalUrl.lastIndexOf('/')+1,255);
+			console.log('select _blob from docs where docId="'+ff+'"');
+			App.using('db').query('dashboard','select _blob from docs where docId="'+ff+'"',function(err,response) {
+				if (response.length>0) {
+					if (response[0]._blob=="") {
+						res.end('Aucun document lié.');
+					} else {
+						var buf = new Buffer(response[0]._blob.split(';base64,')[1], 'base64');
+						res.set('Content-disposition', 'inline; filename="Doc.pdf"');
+						res.set("Content-Type", response[0]._blob.split(';base64')[0].split('data:')[1]);
+						res.end(buf);
+					}
+				} else {
+					var b64=App.upload.toBase64(ff);
+					var buf=new Buffer(b64.split(';base64,')[1], 'base64');
+					res.set('Content-disposition', 'inline; filename="Doc.pdf"');
+					res.set("Content-Type", b64.split(';base64')[0].split('data:')[1]);
+					res.end(buf);
+				}
+			});
+		});			
 	}
 };
 
