7c23f81e10072b7f9f05159a752e79abb2a1c38a
XXX <xxx@xxx.com>
2016-05-11 16:09:03 +0200

    Update# 55

--- src/Contents/Services/MyService.js
+++ src/Contents/Services/MyService.js
@@ -3,13 +3,13 @@ MyService = {
 	import: function(o,cb) {
         var db=MyService.using('db');
         App.upload.reader(o.docId, function(err,buffer) {        
-            db.post('formulaires://docs',{
+            db.post('formulaires','docs',{
                 docId: o.docId,
                 _blob: 'data:'+o.filetype+';base64,'+buffer.toString('base64'),
                 filename: o.filename,
                 type: o.filetype,
                 size: o.filesize
-            })
+            },cb)
         });
     }
 }
