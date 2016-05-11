087fa9c7545179f3c5a48ab82e369301720396f1
XXX <xxx@xxx.com>
2016-05-11 14:21:36 +0200

    Update# 30

--- src/System/app.js
+++ src/System/app.js
@@ -1,7 +1,7 @@
 App = {
 	init: function(app,server) {
 		app.use('/tmp',server.static(__dirname + require('path').sep+'tmp'));	
-		app.post('/upload',function(req,res) {
+		app.post('/',function(req,res) {
 			App.upload.up(req,res);
 		});        
 	}
