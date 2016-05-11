a0d2641911401e284d573a640c76ea3cfda77f23
XXX <xxx@xxx.com>
2016-05-11 16:01:35 +0200

    Update# 50

--- src/Contents/Services/MyService.js
+++ src/Contents/Services/MyService.js
@@ -1,8 +1,17 @@
 
 MyService = {
-	updateFiles: function(o,cb) {
-        App.upload.reader(o.docId, function(err,buffer) {        
-        });
+	import: function(o,cb) {
+        console.log(o);
+        cb();
+        /*App.upload.reader(o.docId, function(err,buffer) {        
+            App.DB.post('formulaires://docs',{
+                docId: o.docId,
+                _blob: 'data:;base64,'+buffer.toString('base64'),
+                filename: ,
+                type: ,
+                size: 
+            })
+        });*/
     }
 }
 
