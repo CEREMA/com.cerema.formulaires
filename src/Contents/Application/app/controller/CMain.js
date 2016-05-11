598ccfc50c19e8c00f4b67a92f453f4c5ec0e590
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-11 20:59:13 +0200

    Update# 89

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -38,6 +38,7 @@ App.controller.define('CMain', {
     VDemandeRepro_onshow: function(me) {
 		if (me.ItemID) {
 			if (me.status==1) {
+				if (Auth.User.profiles.indexOf('SUPERUSER')>-1)
 				App.DB.post('formulaires://demandes',{
 					id: me.ItemID,
 					status: 2,
