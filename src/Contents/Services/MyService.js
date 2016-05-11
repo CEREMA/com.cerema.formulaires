ad8072b92ac2204f92e21578f709ed5066f633b4
XXX <xxx@xxx.com>
2016-05-11 16:07:38 +0200

    Update# 54

--- src/Contents/Services/MyService.js
+++ src/Contents/Services/MyService.js
@@ -1,8 +1,9 @@
 
 MyService = {
 	import: function(o,cb) {
+        var db=MyService.using('db');
         App.upload.reader(o.docId, function(err,buffer) {        
-            App.DB.post('formulaires://docs',{
+            db.post('formulaires://docs',{
                 docId: o.docId,
                 _blob: 'data:'+o.filetype+';base64,'+buffer.toString('base64'),
                 filename: o.filename,
