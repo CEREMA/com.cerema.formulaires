929ac8546c8e39e4857b8d1fc830a9215eed49bf
XXX <xxx@xxx.com>
2016-05-11 15:23:48 +0200

    Update# 43

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -28,6 +28,7 @@ App.controller.define('CMain', {
 	},
     OK_onclick: function(me)
     {
+        alert('x');
         App.DB.post('formulaires://demandes',{
             Name: Auth.User.firstname+' '+Auth.User.lastname,
             UserID: Auth.User.uid,
